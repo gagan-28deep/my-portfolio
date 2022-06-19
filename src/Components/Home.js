5f3d75fded9fadf1d65129e557ca0fe7bba530e4
gagan-28deep <gagan_28_deep@outlook.com>
2022-06-19 13:56:33 +0530

    added till projects

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import image from './bernabeu_especial.jpg'
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <main>
+      <img
+         className="absolute object-cover w-full h-full"
+        src={image}
+        alt="Santiago Benrabau"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Hello I am Gagandeep Singh</h1>
+      </section>
+    </main>
+  );
 }
 
-export default Home
+export default Home;
